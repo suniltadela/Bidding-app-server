77a434cabd5480d1d9cd3b60a2aa6e910abaf903
suniltadela <tskumar329@gmail.com>
2024-10-21 23:42:31 +0530

    fourth commit

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.use(cors());
 app.use(express.json());
 
 // DB Connection
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error('MongoDB connection error:', err));
 connectDB();
 // Simple GET request to check if the server is working
 app.get('/', (req, res) => {
